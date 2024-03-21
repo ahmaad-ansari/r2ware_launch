2cb88f31bf6a3da796c386a79bc4df011098caf4
Ahmaad Ansari <ahmaad.ali.ansari@hotmail.com>
2024-03-20 23:38:53 -0400

    Commit

--- r2ware_launch/r2ware_launch_node.py
+++ r2ware_launch/r2ware_launch_node.py
@@ -1,11 +1,20 @@
 import rclpy
 from rclpy.node import Node
 from rosgraph_msgs.msg import Clock
+from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy
 
 class ClockPublisher(Node):
     def __init__(self):
         super().__init__('r2ware_launch_node')
-        self.clock_publisher = self.create_publisher(Clock, '/clock', 10)
+
+        # Define QoS profile for Clock topic
+        qos_profile = QoSProfile(
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+            reliability=QoSReliabilityPolicy.BEST_EFFORT
+        )
+
+        self.clock_publisher = self.create_publisher(Clock, '/clock', qos_profile)
 
         # Create a timer to publish clock data at 100 Hz
         self.timer = self.create_timer(0.01, self.publish_clock)  # 1 / 100 = 0.01
@@ -22,7 +31,7 @@ class ClockPublisher(Node):
         self.clock_publisher.publish(clock_msg)
 
         # Log the published clock time
-        self.get_logger().info('Publishing clock: %s' % current_time)
+        # self.get_logger().info('Publishing clock: %s' % current_time)
 
 def main(args=None):
     rclpy.init(args=args)
